5e030091f9466fbc2911b218d4c121ce5e44a51a
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-03-05 23:01:04 +0700

    feat: add card content in themepreview page and get project by id server action

---
+++ src/app/(protected)/presentation/[presentationId]/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import {DndProvider} from 'react-dnd'
+import { getProjectById } from "@/actions/projects";
+import { themes } from "@/lib/constants";
+import { useSlideStore } from "@/store/useSlideStore";
+import { Loader2 } from "lucide-react";
+import { useTheme } from "next-themes";
+import { redirect, useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+type Props = {};
+
+const Page = (props: Props) => {
+    // TODO: create presentation builder page
+  const { setSlides, setProject, currentTheme, setCurrentTheme } =
+    useSlideStore();
+
+  const params = useParams();
+  const { setTheme } = useTheme();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getProjectById(params.presentationId as string);
+        if (res.status !== 200 || !res.data) {
+          toast.error("Error", {
+            description: "Unable to fetch project",
+          });
+
+          redirect("/dashboard");
+        }
+
+        const findTheme = themes.find(
+          (theme) => theme.name === res.data.themeName
+        );
+        setCurrentTheme(findTheme || themes[0]);
+        setTheme(findTheme?.type === "dark" ? "dark" : "light");
+        setProject(res.data);
+        setSlides(JSON.parse(JSON.stringify(res.data.slides)));
+      } catch (error) {
+        toast.error("Error", {
+          description: "An unexpected error occured",
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader2 className="size-8 animate-spin text-primary" />
+      </div>
+    );
+  }
+
+  return <DndProvider></DndProvider>
+};
+
+export default Page;
---
+++ src/app/(protected)/presentation/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from "next/navigation";
+import React from "react";
+
+const Page = () => {
+  redirect("/dashboard");
+};
+
+export default Page;
